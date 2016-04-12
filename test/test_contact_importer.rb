5680eb4caaec6d6c0c0d05de97db86e85a493eee
Graeme Rouse <graeme@cloudsponge.com>
2016-04-12 15:37:17 -0700

    passing all specs

--- test/test_contact_importer.rb
+++ test/test_contact_importer.rb
@@ -1,17 +1,12 @@
 require 'test_helper'
 
-VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
-  config.hook_into :webmock
-end
-
 class TestContactImporter < Test::Unit::TestCase
   def test_version_exists
     assert Cloudsponge::VERSION
   end
 
   def test_u_p_import
-    VCR.use_cassette("u_p_import", :match_requests_on => [:uri, :host, :path]) do
+    VCR.use_cassette("u_p_import", :match_requests_on => [:method, :host, :path]) do
       importer = Cloudsponge::ContactImporter.new(DOMAIN_KEY, DOMAIN_PASSWORD)
       importer.begin_import('PLAXO', PLAXO[:user], PLAXO[:password])
       contacts = events_wait(importer)
@@ -20,10 +15,10 @@ class TestContactImporter < Test::Unit::TestCase
   end
 
   def test_aol_import
-    VCR.use_cassette("aol_import", :match_requests_on => [:uri, :host, :path]) do
+    VCR.use_cassette("aol_import", :match_requests_on => [:method, :host, :path]) do
       importer = Cloudsponge::ContactImporter.new(DOMAIN_KEY, DOMAIN_PASSWORD)
       resp = importer.begin_import('AOL')
-      if resp[:consent_url]
+      if ENV['request_consent'] && resp[:consent_url]
         puts "Navigate to #{resp[:consent_url]} and complete the authentication process."
         `open '#{resp[:consent_url]}'`
       end
@@ -33,10 +28,10 @@ class TestContactImporter < Test::Unit::TestCase
   end
 
   def test_wl_import
-    VCR.use_cassette("wl_import", :match_requests_on => [:uri, :host, :path]) do
+    VCR.use_cassette("wl_import", :match_requests_on => [:method, :host, :path]) do
       importer = Cloudsponge::ContactImporter.new(DOMAIN_KEY, DOMAIN_PASSWORD)
       resp = importer.begin_import('WINDOWSLIVE')
-      if resp[:consent_url]
+      if ENV['request_consent'] && resp[:consent_url]
         puts "Navigate to #{resp[:consent_url]} and complete the authentication process."
         `open '#{resp[:consent_url]}'`
       end
@@ -46,10 +41,10 @@ class TestContactImporter < Test::Unit::TestCase
   end
 
   def test_auth_import
-    VCR.use_cassette("wl_import", :match_requests_on => [:uri, :host, :path]) do
+    VCR.use_cassette("wl_import", :match_requests_on => [:method, :host, :path]) do
       importer = Cloudsponge::ContactImporter.new(DOMAIN_KEY, DOMAIN_PASSWORD)
       resp = importer.begin_import('YAHOO')
-      if resp[:consent_url]
+      if ENV['request_consent'] && resp[:consent_url]
         puts "Navigate to #{resp[:consent_url]} and complete the authentication process."
         `open '#{resp[:consent_url]}'`
       end
@@ -59,12 +54,10 @@ class TestContactImporter < Test::Unit::TestCase
   end
 
   def test_contacts_with_mailing_addresses
-    VCR.use_cassette("contacts_with_mailing_addresses", :match_requests_on => [:uri, :host, :path]) do
+    VCR.use_cassette("contacts_with_mailing_addresses", :match_requests_on => [:method, :host, :path]) do
       importer = Cloudsponge::ContactImporter.new(DOMAIN_KEY, DOMAIN_PASSWORD, nil, {"include" => "mailing_address"})
-      #importer.begin_import('PLAXO', PLAXO[:user], PLAXO[:password])
-      #contacts = events_wait(importer)
       resp = importer.begin_import('AOL')
-      if resp[:consent_url]
+      if ENV['request_consent'] && resp[:consent_url]
         puts "Navigate to #{resp[:consent_url]} and complete the authentication process."
         `open '#{resp[:consent_url]}'`
       end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,8 @@ DOMAIN_KEY = "DOMAIN_KEY"
 DOMAIN_PASSWORD = "DOMAIN_PASSWORD"
 
 PLAXO = {:user => 'u', :password => 'p'}
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/vcr_cassettes"
+  config.hook_into :webmock
+end
